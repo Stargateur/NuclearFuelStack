5fddcafac2123ec18e1f17808cbb788d15f48ad5
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2022-09-14 21:53:05 +0200

    0.0.0

---
+++ data.lua
@@ -0,0 +1,2 @@
+data.raw.item["nuclear-fuel"].stack_size =
+  settings.startup["nuclear-fuel-stack-size"].value
---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend
+{
+  {
+    type = "int-setting",
+    name = "nuclear-fuel-stack-size",
+    setting_type = "startup",
+    default_value = 10,
+    minimum_value = 1,
+    order = "a"
+  },
+}
